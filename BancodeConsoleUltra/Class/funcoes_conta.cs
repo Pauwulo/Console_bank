82ba80e7afed1af54f893762f885c1bf2dc3069f
Paulo Vitor <109113692+Pauwulo@users.noreply.github.com>
2022-07-14 04:54:05 -0300

    Console_bank Terminada.

---
+++ BancodeConsoleUltra/Class/funcoes_conta.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace funcoes_conta.Class
+{
+    public class Conta
+    {
+        public string Titular { get; set; }
+        public double Saldo { get; set; }
+
+        public Conta(string titular, double saldo)
+        {
+            Titular = titular;  
+            Saldo = saldo;
+        }
+
+        public void deposito(double valor_deposito)
+        {
+            this.Saldo += valor_deposito;
+            Console.WriteLine($"\nDeposito efetivado com sucesso!\n");
+            Console.WriteLine($"Seu salto atual é: R$ {this.Saldo}\n");
+            Console.WriteLine("Pressione Enter para voltar\n");
+            Console.ReadLine();
+        }
+
+        public void deposito_transferencia(double valor_deposito)
+        {
+            this.Saldo += valor_deposito;
+            Console.WriteLine($"\nTransferencia efetivada com sucesso!\n");
+            Console.WriteLine("Pressione Enter para voltar\n");
+            Console.ReadLine();
+        }
+
+        public bool transferencia(double valor_transferencia)
+        {
+            // Verificar se existe saldo na conta
+            if (this.Saldo - valor_transferencia < 0)
+            {
+                return true;
+            }
+            else
+            {
+                this.Saldo -= valor_transferencia;
+                return false;
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Titular: {this.Titular}");
+            sb.AppendLine($"Saldo: R${this.Saldo.ToString("F2", CultureInfo.InvariantCulture)}");
+            return sb.ToString();
+
+        }
+
+    }
+}
---
+++ BancodeConsoleUltra/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using funcoes_conta.Class;
+
+namespace Console_bank;
+
+class Program
+{
+
+    public static void Main()
+    {
+        Conta contaPrincipal = new Conta("Paulo Vitor", 500.00);
+        Conta contaSegundaria = new Conta("Raissa Ribeiro", 100.00);
+
+        inicio:
+        Console.Clear();
+        Console.WriteLine($"***Bem vindo ao inter de texto {contaPrincipal.Titular}***\n");
+        Console.WriteLine($"O saldo atual de sua conta é: R$ {contaPrincipal.Saldo}\n");
+        Console.WriteLine("Seleciona a função que deseja utilizar:\n");
+
+
+        Console.WriteLine("1 - Listar Contas \n\n2 - Deposito\n\n3 - Transferencia\n");
+
+        string funcao = Console.ReadLine();
+
+        switch (funcao){
+            case "1":
+                Console.Clear();
+                Console.WriteLine(contaPrincipal);
+                Console.WriteLine(contaSegundaria);
+                Console.WriteLine("Pressione enter para voltar;");
+                Console.ReadLine();
+                goto inicio;
+                break;
+
+                case "2":
+                Console.Clear();
+                Console.WriteLine("Digite o valor do deposito:\n");
+                Console.Write("R$ ");
+                double valor_deposito = double.Parse(Console.ReadLine());
+                contaPrincipal.deposito(valor_deposito);
+                goto inicio;
+                break;
+                
+                case "3":
+                Console.Clear();
+                Console.WriteLine($"Saldo disponivel para transferencia: R${contaPrincipal.Saldo}\n");
+                Console.WriteLine("Digite o valor da transferencia:\n");
+                Console.Write("R$ ");
+                double valor_transferencia = double.Parse(Console.ReadLine());
+                bool saldo_insuficiente = contaPrincipal.transferencia(valor_transferencia);
+                if (saldo_insuficiente)
+                {
+                    Console.WriteLine("\nSaldo insuficiente\n");
+                    Console.ReadLine();
+                    goto inicio;
+                }
+                else
+                {
+                    contaSegundaria.deposito_transferencia(valor_transferencia);
+                    goto inicio;
+                }
+        }
+    }
+}
